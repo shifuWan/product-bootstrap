9f815c89bf6f8d2a9b921fe4b19cb504be7bd7b2
shifuWan <almertamal@gmail.com>
2025-08-14 19:40:13 +0700

    refactor: update User model and database schema for username and ULID support
    - Changed 'name' attribute to 'username' in User model and UserFactory.
    - Updated migration to use ULID for user IDs and made 'username' unique.
    - Adjusted DatabaseSeeder to create a user with 'username' instead of 'name'.
    
    These changes improve user identification and align with the new database structure.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,10 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->ulid('id')->primary();
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -29,7 +28,7 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignUlid('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
